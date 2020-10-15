1c1147c2f1e463f696949a080f1402ea4acb0247
nathanjessen <nathan.jessen@gmail.com>
2020-10-14 21:54:35 -0600

    Use gulp setup to run jekyll locally

---
+++ gulpfile.babel.js
@@ -0,0 +1,54 @@
+import browserSync from "browser-sync";
+import spawn from "cross-spawn";
+import { series, task, watch } from "gulp";
+
+const SITE_ROOT = "./_site";
+
+// Fix for Windows compatibility
+const jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+const isDevelopmentBuild = process.env.NODE_ENV === "development";
+
+task("buildJekyll", () => {
+  browserSync.notify("Building Jekyll site...");
+
+  const args = ["exec", jekyll, "build"];
+
+  if (isDevelopmentBuild) {
+    args.push("--incremental");
+  }
+
+  return spawn("bundle", args, { stdio: "inherit" });
+});
+
+task("startServer", () => {
+  browserSync.init({
+    files: [SITE_ROOT + "/**"],
+    open: "local",
+    port: 4000,
+    server: {
+      baseDir: SITE_ROOT,
+      serveStaticOptions: {
+        extensions: ["html"],
+      },
+    },
+  });
+
+  watch(
+    [
+      "**/*.css",
+      "**/*.html",
+      "**/*.js",
+      "**/*.md",
+      "**/*.markdown",
+      "!_site/**/*",
+      "!node_modules/**/*",
+    ],
+    { interval: 500 },
+    buildSite
+  );
+});
+
+const buildSite = series("buildJekyll");
+
+exports.serve = series(buildSite, "startServer");
+exports.default = series(buildSite);
